fc37e674f0729f46cd58394a4d9265f1ec69c06f
slicky1 <okunonke@gmail.com>
2021-07-11 22:17:30 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+  
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(ary)
+      sorted = @word.split("").sort
+      ary.select {|et| et.split("").sort == sorted}
+    end
+  end
